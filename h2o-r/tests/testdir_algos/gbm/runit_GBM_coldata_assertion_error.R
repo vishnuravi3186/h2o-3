638595931dfa4756554f983b1c3df828c183a884
Michal Raška <michal.raska@gmail.com>
2017-08-12 21:53:17 +0200

    [PUBDEV-3847] - removed <- usage in method call params

--- h2o-r/tests/testdir_algos/gbm/runit_GBM_coldata_assertion_error.R
+++ h2o-r/tests/testdir_algos/gbm/runit_GBM_coldata_assertion_error.R
@@ -17,9 +17,9 @@ test.gbm.coldata.assertion_error <- function () {
     min_split_improvement <- 1e-04
 
     model <- h2o.gbm(x = myX, y = myY, training_frame = data, model_id = "amodel", ntrees = ntrees,
-    max_depth <- max_depth , min_rows = min_rows, learn_rate = learn_rate,
-    sample_rate <- sample_rate , col_sample_rate_per_tree = col_sample_rate_per_tree ,
-    nfolds <- nfolds, min_split_improvement = min_split_improvement)
+    max_depth = max_depth , min_rows = min_rows, learn_rate = learn_rate,
+    sample_rate = sample_rate , col_sample_rate_per_tree = col_sample_rate_per_tree ,
+    nfolds = nfolds, min_split_improvement = min_split_improvement)
 }
 doTest("Testing GBM Coldata, there should not be an AssertionError", test.gbm.coldata.assertion_error)
 
